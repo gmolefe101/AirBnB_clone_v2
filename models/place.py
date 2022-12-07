cec2e400725ba672e37c64661ab67b26609a0c94
gontsemolefe <john.konene@gmail.com>
2022-12-07 22:55:51 +0200

    Updated models

--- models/place.py
+++ models/place.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+'''
+    Implementation of the Amenity class
+'''
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Amenity(BaseModel, Base):
+    '''
+        Implementation for the Amenities.
+    '''
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
